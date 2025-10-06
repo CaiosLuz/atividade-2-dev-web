a531c4efd52e7a8b82eb4a5dd5c21c5410c3897d
Caio José <caiojsdl16@gmail.com>
2025-10-06 19:24:10 -0300

    primeiro commit

---
+++ 2025_2-AS403TSN3-WebFrontEnd-main/AulaAngular/src/app/listar-cliente/listar-cliente.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Cliente } from '../models/cliente';
+
+@Component({
+  selector: 'app-listar-cliente',
+  standalone: false,
+  templateUrl: './listar-cliente.html',
+  styleUrl: './listar-cliente.css'
+})
+export class ListarCliente {
+  mostrarTabela: boolean = true;
+  nome: string = '';
+  clienteSelecionado: Cliente | null = null;
+  clientes: Cliente[] = [
+    {
+      id: 1, nome: 'Ana', celular: '99999-9999',
+      cidade: 'São Paulo'
+    },
+    {
+      id: 2, nome: 'Bruno', celular: '88888-8888',
+      cidade: 'Rio de Janeiro'
+    },
+    {
+      id: 3, nome: 'Carlos', celular: '77777-7777',
+      cidade: 'Belo Horizonte'
+    }
+  ];
+}
